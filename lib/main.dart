af3c8b03b771e8044be7346dd6a5757985759c1a
nasim60009 <mdnasimnasim2007@gmail.com>
2025-01-19 16:14:59 +0600

    2st chenge

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar:AppBar(backgroundColor:Colors.green,),);
   }
 }
 
